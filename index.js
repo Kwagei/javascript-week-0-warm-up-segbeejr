9b9d08824cdcc60ad93003ced2250c549a9c8aab
N. Omalley Segbee Jr <segbeejr67@gmail.com>
2023-06-01 18:57:08 +0000

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,58 @@
+// Problem 1:
+// Declare two variables, "x" and "y", and assign them numerical values. 
+// Use the modulus operator to calculate the remainder when "x" is divided by "y". 
+// Print the result.
+
+// Answer:
+let x = 15;
+let y = 4;
+let result = x % y;
+console.log(result);
+
+
+
+
+// Problem 2: 
+// Declare a variable called "name" and assign it a string value of your choice. 
+// Print the value of the variable to the console.
+
+
+// Answer:
+let name = "His Exellency, Rev. Dr. Nathan Omalley Segbee Jr."
+console.log(name);
+
+
+
+// Problem 3: 
+// Declare two variables, "num1" and "num2", and assign them any numerical values. 
+// Calculate the sum of the two variables and store the result in a new variable called "sum". 
+// Print the value of the "sum" variable.
+
+// Answer:
+let num1 = 25;
+let num2 = 35;
+let sum = num1 + num2;
+console.log(sum);
+
+
+
+// Problem 4: 
+// Declare a variable called "radius" and assign it a numerical value. 
+// Calculate the area of a circle using the formula A = πr^2 and store the result 
+// in a variable called "area". Print the value of the "area" variable.
+
+// Answer:
+let radius = 12;
+let area = 3.14159 * radius * radius;
+console.log(area);
+
+
+
+// Problem 5: 
+// Declare a variable called "num" and assign it a numerical value. 
+// Increment the value of "num" by 5 and print the updated value.
+
+// Answer:
+let num = 10;
+num = num + 5;
+console.log(num);
